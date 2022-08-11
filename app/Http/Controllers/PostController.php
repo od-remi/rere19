8b4e2e8c6ce9889b1e27dfaf23219b1230dde57f
od-remi <remisougo@gmail.com>
2022-08-12 04:43:06 +0900

    [add]記事一覧画面

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostController extends Controller
+{
+    /**
+ * Post一覧を表示する
+ * 
+ * @param Post Postモデル
+ * @return array Postモデルリスト
+ */
+public function index(Post $post)
+{
+    return view('index')->with(['posts'=> $post->get()]);
+    
+}
+
+}
+
+
