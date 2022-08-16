dbbd8584d32cdf90357b62fc001d0000cccc8876
od-remi <remisougo@gmail.com>
2022-08-16 21:37:29 +0900

    paginate

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,20 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Post;
+use Illuminate\Http\Request;
+
 
 class PostController extends Controller
 {
-    /**
- * Post一覧を表示する
- * 
- * @param Post Postモデル
- * @return array Postモデルリスト
- */
+    
+
 public function index(Post $post)
 {
-    return view('index')->with(['posts'=> $post->get()]);
+    return view('index')->with(['posts' => $post->getPaginateByLimit(1)]);
     
 }
 
